09b58c9e683533c93e90bce68180a3e74683c5e9
ekenecf <ekens2u@gmail.com>
2022-11-11 00:15:59 +0100

    Chain multiple midleware

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -1,73 +1,91 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/../toursFolder/tours.json`));
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../toursFolder/tours.json`)
+);
 
 exports.checkID = (req, res, next, val) => {
-    // console.log(`Tour id is ${val}`);
-    if (+(req.params.id) > tours.length) {
-        return res.status(404).json({
-            status: 'Not found',
-            message: 'Invalid search ID'
-        })
-    }
-    next();
-}
+  // console.log(`Tour id is ${val}`);
+  if (+req.params.id > tours.length) {
+    return res.status(404).json({
+      status: "Not found",
+      message: "Invalid search ID",
+    });
+  }
+  next();
+};
+
+exports.checkBody = (req, res, next) => {
+  // console.log("Hello from the middleware" );
+  const reqBody = req.body;
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: "failed",
+      message: "Missing name or price",
+    });
+  }
+  next();
+};
 
 exports.getAllTour = (req, res) => {
-    //We can access the req.requestTime inside this function ie
-    res.status(200).json({
-        status: "success",
-        requestedAt: req.requestTime,
-        numberOfTours: tours.length,
-        data: {
-            tours,
-        }
-    })
-}
+  //We can access the req.requestTime inside this function ie
+  res.status(200).json({
+    status: "success",
+    requestedAt: req.requestTime,
+    numberOfTours: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
 
 exports.getTour = (req, res) => {
-    // console.log(+(req.params.id))
-    const id = +(req.params.id);
-    const tour = tours.find(el => el.id === id)
-    // if (id > tours.length) {
-    // if (!tour) {
-    //     return res.status(404).json({
-    //         status: 'Not found',
-    //         message: 'Invalid search ID'
-    //     })
-    // }
-    res.status(200).json({
-        status: "success",
-        data: {
-            tour,
-        }
-    });
-}
+  // console.log(+(req.params.id))
+  const id = +req.params.id;
+  const tour = tours.find((el) => el.id === id);
+  // if (id > tours.length) {
+  // if (!tour) {
+  //     return res.status(404).json({
+  //         status: 'Not found',
+  //         message: 'Invalid search ID'
+  //     })
+  // }
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour,
+    },
+  });
+};
 
 exports.updateTour = (req, res) => {
-    // const id = +(req.params.id);
-    // const tour = tours.find(el => el.id === id)
-    
-    res.status(200).json({
-        status: "success",
-        data: {
-            tour: 'Tour Updated'
-        }
-    });
-}
+  // const id = +(req.params.id);
+  // const tour = tours.find(el => el.id === id)
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour: "Tour Updated",
+    },
+  });
+};
 
 exports.createTour = (req, res) => {
-    // console.log(req.body);
-    const newId = tours[tours.length - 1].id + 1
-    const newTour = Object.assign({id: newId}, req.body);
-    tours.push(newTour);
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
 
-    fs.writeFile(`${__dirname}/toursFolder/tours.json`, JSON.stringify(tours), err => {
-        res.status(201).json({
-            status: 'success',
-            data: {
-                newTour
-            }
-        })
-    })
-}
+  fs.writeFile(
+    `${__dirname}/toursFolder/tours.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          newTour,
+        },
+      });
+    }
+  );
+};
--- routes/tourRoute.js
+++ routes/tourRoute.js
@@ -7,7 +7,9 @@ const router = express.Router();
 // You can use this middleware(router.param) to check for Invalid ID's; Also note it accepts up to 4 arguments
 router.param('id', tourControllers.checkID);
 
-router.route('/').post(tourControllers.createTour).get(tourControllers.getAllTour);
+router.route('/')
+.post(tourControllers.checkBody, tourControllers.createTour)
+.get(tourControllers.getAllTour);
 router.route('/:id').get(tourControllers.getTour).patch(tourControllers.updateTour);
 
 module.exports = router;
