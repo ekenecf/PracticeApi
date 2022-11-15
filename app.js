04bd8221a0cd4fc6e3e07466296e828a3c17073f
ekenecf <ekens2u@gmail.com>
2022-11-15 18:42:18 +0100

    set up mongodb and installed mongoose

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 //Everything that has nothing to do with express should be done outside the app.js file ie in the server.js file
 
-const express = require('express');
-const morgan = require('morgan');
-const tourRouter = require('./routes/tourRoute');
-const userRouter = require('./routes/userRoute');
+const express = require("express");
+const morgan = require("morgan");
+const tourRouter = require("./routes/tourRoute");
+const userRouter = require("./routes/userRoute");
 
 const app = express();
 
 // Middleware is a fxn that modifies the incoming request data
 //This middleware is used when we want to make post requests
 app.use(express.json());
-// Morgan middleware is used to log requests to the console
-app.use(morgan('dev'));
+
+if (process.env.NODE_ENV === "development") {
+  // Morgan middleware is used to log requests to the console
+  app.use(morgan("dev"));
+}
 
 //To serve static files we use an inbuilt middleware called express.ststic(with the directory) ie
 // Then you open the route with server/publicContent it http://localhost:3000/index.html
@@ -20,20 +23,20 @@ app.use(express.static(`${__dirname}/public`));
 // To create our own middleWare functions, we must use app.use to access the middleware eg
 // All middlewares execute based on the order they are called. Also with next(), it makes the next middleware run cos without it, the next wont run.
 app.use((req, res, next) => {
-    // console.log("Hello from the middleware" );
-    next();
+  // console.log("Hello from the middleware" );
+  next();
 });
 
-// To create another middleware, lets use the req.requestTime
+// To create another middleware, we can use the req.requestTime
 app.use((req, res, next) => {
-    req.requestTime = new Date().toISOString();
-    next();
+  req.requestTime = new Date().toISOString();
+  next();
 });
 
 // Routes
 // Parent route using mounting which in this case is the tourRouter and userRouter
-app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/users', userRouter);
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
 
 // app.get('/api/v1/tours', getAllTour);
 // app.get('/api/v1/tours/:id', getTour);
